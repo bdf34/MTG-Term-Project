11d9e891c976c2f761273c0f3d6f31119fdcd9b9
harvp <69948085+harvp@users.noreply.github.com>
2020-10-16 00:37:32 -0400

    First pass at generating input form and submit button
    On collection entry

---
+++ MTGApp/CollectionEntry.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CollectionEntry.aspx.cs" Inherits="MTGApp.CollectionEntry" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Collection Entry</title>
+    <style type="text/css">
+        #TextArea1 {
+            height: 500px;
+            width: 600px;
+        }
+    </style>
+</head>
+<body>
+    <h2>Collection Mass Entry</h2>
+    Use this form to enter your collection
+    <br /><br />
+
+    <form id="MassEntryBox" runat="server">
+        <asp:PlaceHolder ID="TextBox" runat="server" />
+        <br />
+        <asp:PlaceHolder ID="ControlContainer" runat="server"/>
+    </form>
+    <article id="validation output" style="float: left; width:20%">
+        Test Data.
+    </article>
+</body>
+</html>
---
+++ MTGApp/CollectionEntry.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Web.Services.Description;
+using System.Configuration;
+using Microsoft.Data.SqlClient;
+using System.Text;
+using System.Xml.Linq;
+
+
+namespace MTGApp
+{
+    public partial class CollectionEntry : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HtmlTextArea CollectionEntry = new HtmlTextArea();
+            CollectionEntry.ID = "entry-box";
+            CollectionEntry.InnerHtml = "Enter your cards here...";
+            CollectionEntry.Attributes.Add("style", "float: left;  height: 80%; width: 50%;");
+            TextBox.Controls.Add(CollectionEntry);
+
+
+            HtmlButton SubmitButton = new HtmlButton();
+            SubmitButton.ID = "btn-submit";
+            SubmitButton.InnerHtml = "Submit";
+            SubmitButton.ServerClick += new System.EventHandler(this.SubmitButton_Click);
+            ControlContainer.Controls.Add(SubmitButton);
+            
+        }
+
+        void SubmitButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
