c9e6565220d4e674c1188062f5b73314aa4a58b9
harvp <69948085+harvp@users.noreply.github.com>
2020-11-29 14:51:36 -0500

    Progress on deck rating

---
+++ MTGApp/DeckViewer.aspx
@@ -0,0 +1,86 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DeckViewer.aspx.cs" Inherits="MTGApp.DeckViewer" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            Choose your deck:
+            <select id="Select1" multiple="false" runat="server"/>
+            <asp:PlaceHolder ID="ConfirmDeck" runat="server" />
+            <asp:PlaceHolder ID="DeckDisplay" runat="server" />
+            <asp:PlaceHolder ID="ProblemCardDisplay" runat="server" />
+            <asp:PlaceHolder ID="ProblemCardEntry" runat="server" />
+            <asp:PlaceHolder ID="DeckEntry" runat="server" />
+            <asp:PlaceHolder ID="SuggestCardButton" runat="server" />
+            <asp:PlaceHolder ID="SuggestedCard" runat="server" />
+
+
+         <asp:Repeater ID="Repeater1" runat="server" 
+          DataSourceID="SqlDataSource1">
+          <HeaderTemplate>
+              <table>
+              <tr>
+                 <th>Name</th>
+                 <th>Description</th>
+              </tr>
+          </HeaderTemplate>
+
+          <ItemTemplate>
+          <tr>
+              <td bgcolor="#CCFFCC">
+                <asp:Label runat="server" ID="Label1" 
+                    text='<%# Eval("userID") %>' />
+              </td>
+              <td bgcolor="#CCFFCC">
+                  <asp:Label runat="server" ID="Label2" 
+                      text='<%# Eval("deckName") %>' />
+              </td>
+          </tr>
+          </ItemTemplate>
+
+          <AlternatingItemTemplate>
+          <tr>
+              <td >
+                <asp:Label runat="server" ID="Label3" 
+                    text='<%# Eval("userID") %>' />
+              </td>
+              <td >
+                 <asp:Label runat="server" ID="Label4" 
+                     text='<%# Eval("deckName") %>' />
+              </td>
+          </tr>
+          </AlternatingItemTemplate>
+
+          <FooterTemplate>
+              </table>
+          </FooterTemplate>
+      </asp:Repeater>
+
+                  <asp:SqlDataSource 
+          ConnectionString=
+              "<%$ ConnectionStrings:myServer %>"
+          ID="SqlDataSource1" runat="server" 
+          SelectCommand="SELECT [deckID], [userID], 
+              [deckName] FROM [DeckLIst]">
+      </asp:SqlDataSource>
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ MTGApp/DeckViewer.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Diagnostics;
+using System.Web.Services.Description;
+using System.Configuration;
+using Microsoft.Data.SqlClient;
+using System.Text;
+using System.Xml.Linq;
+
+namespace MTGApp
+{
+    public partial class DeckViewer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+            if (!IsPostBack)
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder
+                {
+                    DataSource = "hrpsvr.database.windows.net",
+                    UserID = "hrpzip",
+                    Password = "DBMProject1!",
+                    InitialCatalog = "MagicDB"
+                };
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    string queryBuild = "SELECT * FROM Decks WHERE userID = 22;";
+                    SqlDataAdapter command = new SqlDataAdapter(queryBuild, connection);
+                    connection.Open();
+
+                    DataSet ds = new DataSet();
+                    command.Fill(ds, "DeckList");
+
+                    Select1.DataSource = ds;
+                    Select1.DataTextField = "deckName";
+                    Select1.DataValueField = "deckId";
+                    Select1.DataBind();
+                }
+
+
+                HtmlButton DeckButton = new HtmlButton
+                {
+                    ID = "Submitter",
+                    InnerHtml = "Submit"
+                };
+                DeckButton.Attributes.Add("style", "float: left;");
+                DeckButton.ServerClick += new EventHandler(DeckButton_Click);
+                ConfirmDeck.Controls.Add(DeckButton);
+
+
+
+
+
+            }
+            
+
+
+            //create deck viewer
+            //create deck entry
+
+            //problem card display
+
+            //problem card entry
+
+
+            //create suggest button
+            /*
+            HtmlButton SuggestButton = new HtmlButton
+            {
+                ID = "Submitter",
+                InnerHtml = "Submit"
+            };
+            SuggestButton.Attributes.Add("style", "float: left;");
+            SuggestButton.ServerClick += new EventHandler(SuggestCardButton_Click);
+            SuggestCardButton.Controls.Add(SuggestButton);
+
+            */
+        }
+
+        public void SuggestCardButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void DeckButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
